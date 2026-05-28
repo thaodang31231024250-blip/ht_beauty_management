08246ba4ad9dc1afbc5547a0a53d45c7bc8e8b1c
thaodang31231024250-blip <thaodang.31231024250@st.ueh.edu.vn>
2026-05-28 11:40:40 +0700

    Delete models directory

--- models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import beauty_room
-from . import crm_lead
-from . import hr_employee
-from . import res_partner
--- models/crm_lead.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class CrmLead(models.Model):
-    _inherit = 'crm.lead'
-
-    zalo_link = fields.Char(string='Zalo Link', tracking=True)
